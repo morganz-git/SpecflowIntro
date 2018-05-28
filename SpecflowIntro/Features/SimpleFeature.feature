cccc4eb617e938acae0833fc188c951e79157fe4
morgan <detect.z@gmail.com>
2018-05-28 16:20:09 +1200

    Add project files.

---
+++ SpecflowIntro/Features/SimpleFeature.feature
@@ -0,0 +1,24 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@smokeTest
+Scenario: Add two numbers
+	Given I have entered 50 into the calculator
+	And I have entered 70 into the calculator
+	When I press add 
+	Then the result should be 120 on the screen
+
+
+Scenario: Create a new employee with mandatary details
+#Given I have opened my application
+#Then I should see employee details page
+When I fill all the mandatory details in form
+| Name    | Age | Phone     | Email        |
+| karthik | 28  | 112548523 | ka@gmail.com |
+#And I click the save button
+#Then I should see all the details saved in my appliation and DB
+
+
+
---
+++ SpecflowIntro/StepDefination/SampleFeatureSteps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SpecflowIntro.StepDefination
+{
+    [Binding]
+    class SampleFeatureSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            Console.WriteLine(number);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+           Console.WriteLine("Press the add button!");
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int result)
+        {
+            if (result==120)
+            {
+                Console.WriteLine("Pass!");
+            }
+            else
+            {
+                Console.Write("fail");
+                throw new Exception("Fail");
+            }
+        }
+
+    }
+}
