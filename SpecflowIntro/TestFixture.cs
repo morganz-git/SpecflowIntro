5b322a802cffaf46f1969e9e127ac1a2d5aec10d
morgan <detect.z@gmail.com>
2018-06-01 12:05:42 +1200

    add Hooks(TestFixture) to the test: 1 BeforeFeature 2 BeforeScenario

---
+++ SpecflowIntro/TestFixture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecflowIntro
+{
+    [Binding]
+    public sealed class TestFixture
+    {
+        // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+        [BeforeFeature]
+        public static  void BeforeFeature()
+        {
+            Console.WriteLine("BeforeFeature");
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            //TODO: implement logic that has to run before executing each scenario
+            Console.WriteLine("BeforeScenario");
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            //TODO: implement logic that has to run after executing each scenario
+            Console.WriteLine("AfterScenario");
+        }
+    }
+}
